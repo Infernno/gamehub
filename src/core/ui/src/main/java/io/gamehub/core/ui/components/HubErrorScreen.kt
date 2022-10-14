f38fbfa8481ff77ab52c457a1d6b40bf6b5e1ea4
Infernno <7915327+Infernno@users.noreply.github.com>
2022-10-14 16:41:18 +0700

    Add lottie animation

--- src/core/ui/src/main/java/io/gamehub/core/ui/components/HubErrorScreen.kt
+++ src/core/ui/src/main/java/io/gamehub/core/ui/components/HubErrorScreen.kt
@@ -1,23 +1,45 @@
 package io.gamehub.core.ui.components
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Error
-import androidx.compose.material3.Icon
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.rememberLottieComposition
+import io.gamehub.core.ui.R
 
 @Composable
 fun HubErrorScreen(
     modifier: Modifier = Modifier,
 ) {
-    Icon(
-        modifier = modifier
-            .fillMaxSize()
-            .wrapContentSize(Alignment.Center),
-        imageVector = Icons.Default.Error,
-        contentDescription = null
+    val composition by rememberLottieComposition(
+        LottieCompositionSpec.RawRes(R.raw.lottie_error_animation)
     )
+
+    Column(
+        modifier = modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        LottieAnimation(composition)
+        Text(
+            text = stringResource(R.string.error_title),
+            style = MaterialTheme.typography.titleLarge
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(
+            text = stringResource(R.string.error_description),
+            style = MaterialTheme.typography.bodyMedium
+        )
+    }
 }
